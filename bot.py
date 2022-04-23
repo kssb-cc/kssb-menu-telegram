c4f667c9ae93123e5bd5e36610d00d2aca386f05
braillescreen <patrick@pwry.net>
2022-04-23 18:18:33 -0500

    It works! Mostly. Need to make it send back the menu but other commands (/help and handling of unknown ones) work good.

--- bot.py
+++ bot.py
@@ -9,17 +9,24 @@ updater = Updater("5371005183:AAFmbWVMP3_muBdSBvKwDQclf58G-jSL0Yk", use_context=
 
 
 def start(update: Update, context: CallbackContext):
-	update.message.reply_text("Welcome to the bot to get the KSSB menu!\n\nYou can type \"/menu\" to get the menu.")
+	update.message.reply_text("Welcome to the bot to get the KSSB menu!\n\nYou can type \"/menu\" to get the menu for the week.\n\nIf you have any complaints, questions etc, I am on Twitter @BrailleScreen.")
 
 def help(update: Update, context: CallbackContext):
-	update.message.reply_text("The only command available at the moment is \"/menu\".")
+	update.message.reply_text("The only command available at the moment is \"/menu\". If typed, it will send back KSSB's lunch menu for the week.")
+
+def menu(update: Update, context: CallbackContext):
+	menu = ""
+	update.message.reply_text(m)
 
 def unknown(update: Update, context: CallbackContext):
-	update.message.reply_text("Sorry; I don't know this command. Please type \"/help\" for a listing of available commands.")
+	update.message.reply_text("Sorry; I don't know this command. Please type \"/help\" for a listing of available ones.")
 
 
-updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
-updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown)) # Filters out unknown commands
+updater.dispatcher.add_handler(CommandHandler("help", help))
+updater.dispatcher.add_handler(CommandHandler("menu", menu))
+
+#Handle unknown commands.
+updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
 
 
 updater.start_polling()
